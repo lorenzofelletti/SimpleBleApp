20f573abc6a39ff2215babf6ee8f669a464f3ebe
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2022-10-22 17:32:44 +0200

    removed unused code

--- app/src/main/java/com/lorenzofelletti/simplebleapp/MainActivityUtilities.kt
+++ app/src/main/java/com/lorenzofelletti/simplebleapp/MainActivityUtilities.kt
@@ -4,41 +4,7 @@ import android.annotation.SuppressLint
 import android.bluetooth.BluetoothGattCharacteristic
 import android.bluetooth.BluetoothGattDescriptor
 import android.bluetooth.BluetoothGattService
-import android.view.View
 import com.lorenzofelletti.simplebleapp.ble.gattserver.GattServerManager
-
-private val DEBUG = BuildConfig.DEBUG
-
-/**
- * A function that builds an [android.view.View.OnClickListener] for starting/stopping the BLE server having
- * two different behaviors depending on whether a server is already running or not.
- *
- * @param initialServerState The initial state of the server.
- * @param startServerAction A lambda that starts the server.
- * @param stopServerAction A lambda that stops the server.
- * @return An [android.view.View.OnClickListener] that starts the server if it is not already
- */
-fun startServerOnClickListenerBuilder(
-    initialServerState: Boolean = false,
-    startServerAction: () -> Unit,
-    stopServerAction: () -> Unit
-): (View) -> Unit {
-    var isServerStarted = initialServerState
-    return fun(v: View) {
-        if (DEBUG) android.util.Log.d(
-            ::startServerOnClickListenerBuilder.name,
-            "${v.javaClass.simpleName}:${v.id} - ${if (isServerStarted) "Stop" else "Start"} Server Button Clicked"
-        )
-
-        isServerStarted = if (isServerStarted) {
-            stopServerAction()
-            false
-        } else {
-            startServerAction()
-            true
-        }
-    }
-}
 
 /**
  * Creates the service and the characteristic to be added to the GATT server and
